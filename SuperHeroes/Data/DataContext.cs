b784c71c3fd2123530ac4d4a3da062843f1c5809
Harpua <harpua123@gmail.com>
2024-05-29 21:52:35 -0500

    2nd commit

---
+++ SuperHeroes/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroes.Models;
+
+namespace SuperHeroes.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<SuperHero> SuperHeroes { get; set; }
+	}
+}
--- SuperHeroes/Program.cs
+++ SuperHeroes/Program.cs
@@ -1,8 +1,16 @@
+global using Microsoft.EntityFrameworkCore;
+global using SuperHeroes.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DBCS"));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
